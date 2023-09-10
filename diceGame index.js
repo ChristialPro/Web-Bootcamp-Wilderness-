186ee22c3f614209bff63a70f60bca7b2e55fd7b
Christial <129101327+ChristialPro@users.noreply.github.com>
2023-09-10 18:00:11 -0500

    Create diceGame index.js

---
+++ diceGame index.js
@@ -0,0 +1,16 @@
+function getRandomArbitrary(1,6) {  
+let randomNumber1 = Math.floor(Math.random() * 6 + 1); 
+let randomNumber2 = Math.floor(Math.random() *6 + 1); 
+
+document.querySelector(".img1").setAttribute("src", "images/dice6" + randomNumber1 + ".png"); 
+document.querySelector(".img2").setAttribute("src", "images/dice6" + randomNumber2 + ".png"); 
+}
+if (randomNumber1 > randomNumber2) { 
+document.querySelector("h1").innerHTML("Player 1 Wins!");
+} 
+if (randomNumber2 > randomNumber1) { 
+document.queryselector("h1").innerHTML("Player 2 Wins!"); 
+}
+else if (randomNumber1 = randomNumber2) {
+ document.queryselector("h1").innerHTML("Draw!")
+}
